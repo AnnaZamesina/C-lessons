76e2206afdb6d38de5af2f7e1e0c5a3b09b8bc06
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-13 22:14:22 +0600

    add task05_array

---
+++ task05_array/Program.cs
@@ -0,0 +1,35 @@
+﻿// Заполнить массив случайными числами и вывести все элементы через 1
+
+
+void PrintEachSenond (int [] col)
+{
+    int index = 0;
+    int lenght = col.Length;
+    
+    while (index<lenght)
+    {
+        // col[index]=new Random().Next(1,10);
+        Console.WriteLine(col[index]);
+        index=index+2;
+    }
+}
+
+void PrintArray (int [] collection)
+{
+    int ind = 0;
+    int lenght = collection.Length;
+    
+    while (ind<lenght)
+    {
+        collection[ind]=new Random().Next(1,10);
+        Console.WriteLine(collection[ind]);
+        ind++;
+    }
+}
+
+int [] array = new int [5];
+PrintArray(array);
+Console.WriteLine();
+PrintEachSenond(array);
+
+
