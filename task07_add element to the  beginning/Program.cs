e8b0d597d11b53acd2312cdc4972f013cb50bc2a
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-14 17:14:20 +0600

    add task with adding an element to the beginning

---
+++ task07_add element to the  beginning/Program.cs
@@ -0,0 +1,15 @@
+﻿// Блок 2. В массив добавляется элемент в начало.
+int[] array = { 3, 78, 9, 234 };
+int l = array.Length;
+int newL = l + 1;
+int[] newArray = new int[newL];
+int ind = 1;
+newArray[0] = new Random().Next(1, 18);
+Console.WriteLine(newArray[0]);
+while (ind < newL)
+{
+    newArray[ind] = array[ind-1];
+    Console.WriteLine(newArray[ind]);
+    ind++;
+}
+
