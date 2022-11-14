5e1f0e8589e1bb47221906cb1f558adbf9ea95fb
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-14 17:28:55 +0600

    add task08 add 4 to the position

---
+++ task08_add element to the position/Program.cs
@@ -0,0 +1,13 @@
+﻿// Блок 3. В массив добавляется элемент в позицию (допустим 4).
+int [] array = { 3, 78, 9, 234 };
+int l = array.Length;
+int count = 0;
+Console.WriteLine("Введите номер позиции");
+int position = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine();
+while (count<l)
+{
+    if (count == position) array[count]=4;
+    Console.WriteLine(array[count]);
+    count++;
+}
