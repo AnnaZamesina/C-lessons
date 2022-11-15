e5af2d0ce0b4e7918f626b9728980ce07e582382
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-15 15:29:23 +0600

    add task14

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,4 +1,4 @@
-﻿// 14. Напишите программу, которая принимает на
+﻿// Напишите программу, которая принимает на
 // вход число и проверяет, кратно ли оно
 // одновременно 7 и 23.
 // 14 -> нет
@@ -8,9 +8,13 @@
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-bool DoubleMultyplicity(int num)
+bool Multyplicity(int num)
 {
-    return (num % 7 == 0 && num % 23 == 0);
+    return num % 7 == 0 && num % 23 == 0;
 }
+Console.WriteLine(Multyplicity(number)? "Да" : "Нет");
 
-Console.WriteLine(DoubleMultyplicity(number));
+
+
+
+
