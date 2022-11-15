3eeafc24d35c265ae582253cc8afa61e5c67a82f
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-15 14:47:13 +0600

    add task12

---
+++ task12/Program.cs
@@ -0,0 +1,17 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool Multyplicity (int num1, int num2)
+{
+    if (num1%num2==0) return true;
+    return false;
+}
+Console.WriteLine ($"{Multyplicity(number1, number2)}, остаток = {number1%number2}");
