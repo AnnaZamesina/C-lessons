3a35eb57d8ad8f6f842405077eaf89b801146709
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-15 15:32:42 +0600

    add task16

--- task16/Program.cs
+++ task16/Program.cs
@@ -11,5 +11,9 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-bool 
+bool Multyplicity(int num1, int num2)
+{
+    return num1==num2*num2 || num2==num1*num1;
+}
+Console.WriteLine(Multyplicity(number1, number2)? "Да" : "Нет");
 
