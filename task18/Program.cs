7f46dd7b3632e9d1cee51c3a6f165f174f95eb1b
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-18 20:16:12 +0600

    int to string task18

--- task18/Program.cs
+++ task18/Program.cs
@@ -3,14 +3,14 @@
 // возможных координат точек в этой четверти (x и y).
 
 Console.WriteLine("Введите номер четверти");
-int quarter = Convert.ToInt32(Console.ReadLine());
+string quarter = Console.ReadLine();
 
-string Range(int number)
+string Range(string number)
 {
-    if (number == 1) return "x > 0, y > 0";
-    if (number == 2) return "x < 0, y > 0";
-    if (number == 3) return "x < 0, y < 0";
-    if (number == 4) return "x > 0, y < 0";
+    if (number == "1") return "x > 0, y > 0";
+    if (number == "2") return "x < 0, y > 0";
+    if (number == "3") return "x < 0, y < 0";
+    if (number == "4") return "x > 0, y < 0";
     return "Некорректный ввод данных";
 
 }
