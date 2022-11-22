ac9494d53459c2bf6e4af1691e5057d4498c9221
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-22 15:10:53 +0600

    add task28

--- task28/Program.cs
+++ task28/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Multyplicity(int num)
+{
+    int multy = 1;
+    for (int i = 1; i <= num; i++)
+    {
+       multy = multy * i; 
+    }
+    return multy;
+}
+int result = Multyplicity(number);
+Console.Write ($"Произведение от 1 до N: {result}");
+
