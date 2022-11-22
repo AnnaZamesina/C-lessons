594387f1bc11f87e4d1aa05287a053cd83c5eb75
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-22 13:36:33 +0600

    add task23

---
+++ task23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+int SumDigits (int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int result = SumDigits (number);
+Console.WriteLine ($"Сумма чисел от 1 до N: {SumDigits(number)}");
