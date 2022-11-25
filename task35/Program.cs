44fbc7bd0b109be1c6d087e4fe48b5d02ba693bc
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-25 15:49:47 +0600

    tasks 35 37

---
+++ task35/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int[] CreateArrayRndint(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] array = CreateArrayRndint(123, 0, 1000);
+PrintArray(array);
+
+int FindTwoDigitsNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] < 100) count++;
+    }
+    return count;
+}
+Console.WriteLine();
+Console.WriteLine ($"Количество двузначных чисел в массиве: {FindTwoDigitsNumber(array)}");
